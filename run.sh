66feca14ee70d1bb53b85502f322524ab083deaa
PiOGEEK <peter@szyszka.us>
2025-03-15 17:18:08 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -88,9 +88,14 @@ $DOCKER build -t thingino-dev .
 [ -d "$BR2_DL_DIR" ] || mkdir -p "$BR2_DL_DIR"
 
 # Run a container in interactive mode and mount the source files in it
-$DOCKER run --rm -it --userns=keep-id \
-	-v $(pwd)/workspace:/home/me \
-	-v $BR2_DL_DIR:/home/me/downloads \
-	thingino-dev:latest
+#$DOCKER run --rm -it --userns=keep-id \
+	#-v $(pwd)/workspace:/home/me \
+	#-v $BR2_DL_DIR:/home/me/downloads \
+	#thingino-dev:latest
+ #fixes docker user error
+ docker run --rm -it --user $(id -u):$(id -g) \
+  -v "$(pwd)/workspace":/home/me \
+  -v "$BR2_DL_DIR":/home/me/downloads \
+  thingino-dev:latest
 
 exit 0
